cb44f337f4c1f7bb2f93b99703276a4d3d1a39b3
nisheedj <nisheedj@thoughtworks.com>
2014-12-27 19:34:01 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('wpWeather', [
+  'wpWeather.service',
+  'wpWeather.controller',
+  'wpWeather.directive',
+  'wpWeather.filter'
+]);
+angular.module('wpWeather.service', []);
+angular.module('wpWeather.controller', []);
+angular.module('wpWeather.directive', []);
+angular.module('wpWeather.filter', []);
